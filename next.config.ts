672fae42ae5a5714533881772030e64ae0172196
Ori Guy Studio <origuy@origuystudio.com>
2025-03-09 20:30:13 +0200

    Refactor distDir in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  distDir: "out",
+  distDir: "build",
 }
 
 export default nextConfig
