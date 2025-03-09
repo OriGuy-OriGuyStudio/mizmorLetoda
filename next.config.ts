b7d4b469dfd41f08a56e16561427f9064e996708
Ori Guy Studio <origuy@origuystudio.com>
2025-03-09 20:14:37 +0200

    Refactor build command in netlify.toml and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
+  distDir: "out",
+}
 
-export default nextConfig;
+export default nextConfig
